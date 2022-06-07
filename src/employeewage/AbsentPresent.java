f68f2af5c315f5b998b2a3a926539bdfd01528ed
msnkittu859 <Vamshikalyan97>
2022-06-07 20:30:46 +0530

    Absent Or Present

---
+++ src/employeewage/AbsentPresent.java
@@ -0,0 +1,9 @@
+package employeewage;
+
+public class AbsentPresent {
+	  public int AbsentPresent() {
+	        int checkPresent = (int)Math.floor(Math.random()*10)%2;
+	        return checkPresent;
+	    }
+
+}
