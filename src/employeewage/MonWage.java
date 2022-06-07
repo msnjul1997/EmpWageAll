cfef8c090eb88766cb4175ff35efe133e68f3d6a
msnkittu859 <Vamshikalyan97>
2022-06-07 20:26:44 +0530

    MonWage Added

---
+++ src/employeewage/MonWage.java
@@ -0,0 +1,10 @@
+package employeewage;
+
+public class MonWage    {
+	  public int wageOfEmp(int day, int wage, int hours) {
+	        int monthlyWage = day * wage * hours;
+	        System.out.println("Monthly wage of employee is " + monthlyWage);
+	        return monthlyWage;
+	    }
+
+}
